03c7d0c9276aa08f178e8b234d5d530ab6cf9137
huy2002 <huy2002109@gmail.com>
2023-05-20 19:21:19 +0700

    feat: add bottom navigation

---
+++ lib/Screens/Screen2.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Screen2());
+
+class Screen2 extends StatelessWidget {
+  const Screen2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
